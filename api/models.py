51742589bb706aafa0a6d70beafb7f231a21e24c
Melissa Murphy <melissa@thoughtfulmonkey.design>
2020-03-01 13:49:47 -0700

    basic admin set up. Moving to base endpoints

--- api/models.py
+++ api/models.py
@@ -4,5 +4,7 @@ from uuid import uuid4
 
 class Player(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
     char_name = models.CharField(max_length=25)
-    base_health = models.IntegerField(5)
+    base_health = models.IntegerField("health points")
